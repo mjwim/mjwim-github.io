0b51c119d54e7c5c32a26b160d6c4edd596dfc11
Matthew Wallis <mattjwallis@gmail.com>
2018-03-12 18:04:30 +0000

    updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// global const Typewriter //
 $( document ).ready(function() {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'],
+    menu: '#myMenu',
+    navigation: true,
+    navigationPosition: 'left',
+    navigationTooltips: ['Home', 'Profile', 'Portfolio', 'Skills'],
+    showActiveTooltip: true
+  });
+
+  var type = document.getElementById('type');
+
+  const typewriter = new Typewriter(type, {
+    loop: true
+  });
+
+  typewriter.typeString('Current focus...')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Codewars Kata')
+    .pauseFor(2500)
+    // .deleteChars(13)
+    // .typeString('www.codewars.com/users/mjwim')
+    .start();
+
 });
